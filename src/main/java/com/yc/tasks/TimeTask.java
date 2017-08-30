28e6e02723348cbddeedab49378007ec4b8bf940
Air <1092318651@qq.com>
2017-08-30 09:22:39 +0800

    Air的上传

--- src/main/java/com/yc/tasks/TimeTask.java
+++ src/main/java/com/yc/tasks/TimeTask.java
@@ -13,6 +13,7 @@ public class TimeTask {
 	
 	 @Scheduled(cron = "0 50 21 ? * ?")
 	public void test1() {
+		 
 		//System.out.println("job 开始执行" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 	}
 	
